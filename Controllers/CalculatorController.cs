632863e4e5ace993c8148ecdf24c5fe9834de2a8
Eiver Romero <a2024112722@estudiantes.upsa.edu.bo>
2025-08-20 19:55:18 -0400

    feat: CalculatorController skeleton

---
+++ Controllers/CalculatorController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CalculatorAPI.Controllers;
+
+public class CalculatorController : Controller
+{
+    // GET
+    public IActionResult Index()
+    {
+        return View();
+    }
+}
